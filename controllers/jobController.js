8534d762c580475da26a72dcdb91c20f9cb00b4e
akshat123436 <akshat123436@gmail.com>
2023-12-12 11:28:59 +0530

    not valid id in validation check itself

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,7 +1,6 @@
 import { nanoid } from "nanoid";
 import Job from "../models/JobModel.js";
 import { StatusCodes } from "http-status-codes";
-import { NotFoundError } from "../errors/customErrors.js";
 let jobs = [
   { id: nanoid(), company: "apple", position: "front-end" },
   { id: nanoid(), company: "samsung", position: "back-end" },
@@ -15,10 +14,6 @@ export const getAllJobs = async (req, res) => {
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
-
-  if (!job) {
-    throw new NotFoundError(`no job with id ${id}`);
-  }
 
   res.status(StatusCodes.OK).json({ job });
 };
@@ -35,11 +30,6 @@ export const updateJob = async (req, res) => {
     new: true,
   });
 
-  if (!updatedJob) {
-    return res
-      .status(StatusCodes.NOT_FOUND)
-      .json({ msg: `no job found with id ${id}` });
-  }
   res.status(StatusCodes.OK).json({ msg: "job modified", job: updatedJob });
 };
 
@@ -47,8 +37,5 @@ export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const removedJob = await Job.findByIdAndDelete(id);
 
-  if (!removedJob) {
-    return res.status(404).json({ msg: `no job found with id ${id}` });
-  }
   res.status(200).json({ msg: "job deleted", job: removedJob });
 };
