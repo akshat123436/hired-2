394f04b638b9318e1d73bc4ad084e5088a27c4b6
akshat123436 <akshat123436@gmail.com>
2023-12-14 18:06:56 +0530

    alljobs fetch logic

---
+++ client/src/components/JobsContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function JobsContainer() {
+  return <div>JobsContainer</div>;
+}
+
+export default JobsContainer;
--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import SearchContainer from "../components/SearchContainer";
+import JobsContainer from "../components/JobsContainer";
+import { useLoaderData } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+import { redirect } from "react-router-dom";
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get("/jobs");
+    return { data };
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+  return null;
+};
 function AllJobs() {
+  const { data } = useLoaderData();
   return (
-    <div>AllJobs</div>
-  )
+    <>
+      <SearchContainer></SearchContainer>
+      <JobsContainer></JobsContainer>
+    </>
+  );
 }
 
-export default AllJobs
+export default AllJobs;
