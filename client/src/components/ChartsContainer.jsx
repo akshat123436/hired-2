420d88e3a884869b338471cb33ac0f05a83a4c7e
akshat123436 <akshat123436@gmail.com>
2023-12-17 15:12:58 +0530

    switch between charts

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+import Wrapper from "../assets/wrappers/ChartsContainer.js";
 
-function ChartsContainer() {
-  return <div>ChartsContainer</div>;
+function ChartsContainer({ data }) {
+  const [barChart, setBarChart] = useState(true);
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart />}
+    </Wrapper>
+  );
 }
 
 export default ChartsContainer;
