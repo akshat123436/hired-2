d8208e5413a1eaa679c99c4d809d25e01d4f011a
akshat123436 <akshat123436@gmail.com>
2023-12-17 09:30:20 +0530

    image cloudinary display and delete

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ import { dirname } from "path";
 import path from "path";
 
 import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 const app = express();
 
