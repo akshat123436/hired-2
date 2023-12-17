41cf43ca412abaebf07cbcc6b4744f206f261e4f
akshat123436 <akshat123436@gmail.com>
2023-12-17 06:38:54 +0530

    profile page setup

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-
+import React from "react";
+import { FormRow } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { useNavigation, Form } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 function Profile() {
+  const { user } = useOutletContext();
+  const { name, lastName, email, location } = user;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
-    <div>Profile</div>
-  )
+    <Wrapper>
+      <Form method="post" className="form" encType="multipart/form-data">
+        <h4 className="form-title"> profile</h4>
+        <div className="form-center">
+          <div className="form-row">
+            <label htmlForm="avatar" className="form-label">
+              Select an image file (max 0.5 MB)
+            </label>
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              className="form-input"
+              accept="image/*"
+            ></input>
+          </div>
+          <FormRow type="text" name="name" defaultValue={name}></FormRow>
+          <FormRow
+            type="text"
+            name="lastName"
+            labelText="last name"
+            defaultValue={lastName}
+          ></FormRow>
+          <FormRow type="email" name="email" defaultValue={email}></FormRow>
+          <FormRow
+            type="text"
+            name="location"
+            defaultValue={location}
+          ></FormRow>
+          <button
+            className="btn btn-block form-btn"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submitting" : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 }
 
-export default Profile
+export default Profile;
--- server.js
+++ server.js
@@ -7,17 +7,30 @@ import morgan from "morgan";
 import mongoose from "mongoose";
 //routers
 import jobRouter from "./routes/jobRouter.js";
-import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import authRouter from "./routes/authRouter.js";
 import userRouter from "./routes/userRouter.js";
+
+//middlewares
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
+
+//public
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+
 import cookieParser from "cookie-parser";
+
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/api/v1/test", (req, res) => {
   res.json({ msg: "test link" });
 });
@@ -27,7 +40,9 @@ app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
 });
+
 app.use(errorHandlerMiddleware);
+
 const port = process.env.PORT || 5100;
 try {
   await mongoose.connect(
