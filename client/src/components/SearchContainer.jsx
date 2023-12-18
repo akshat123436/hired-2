34f30159ac8274b84434e96178b0219b338967c4
akshat123436 <akshat123436@gmail.com>
2023-12-18 05:31:43 +0530

    debounce....delay in search after typing

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -11,6 +11,18 @@ function SearchContainer() {
   const { searchValues } = useAllJobsContext();
   const { search, jobStatus, jobType, sort } = searchValues;
   const submit = useSubmit();
+
+  const debounce = (onChange) => {
+    let timeout;
+    return (e) => {
+      const form = e.currentTarget.form;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        onChange(form);
+      }, 2000);
+    };
+  };
+
   return (
     <Wrapper>
       <Form className="form">
@@ -20,9 +32,9 @@ function SearchContainer() {
             type="search"
             name="search"
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form);
-            }}
+            onChange={debounce((form) => {
+              submit(form);
+            })}
           />
           <FormRowSelect
             labelText="job status"
